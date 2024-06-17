104bcc7b9b537c2e4885df7f9461055391b645cd
RichardLiu <97330937+RichardLiuda@users.noreply.github.com>
2024-06-17 10:29:46 +0800

    Create test.cpp

---
+++ C/Object-Oriented Programming/test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+typedef double db;
+#define int long long
+
+void QAQ()
+{
+    cout << (int)'|' << endl;
+}
+
+signed main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("in.in", "r", stdin);
+    //     freopen("out.out", "w", stdout);
+    // #endif
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    //    cin>>t;
+    while (t--)
+    {
+        QAQ();
+    }
+    return 0;
+}
